447c4723ecb8665eede5de57ebfcffd0cff29a31
Danny Sanchez <dsanchez910@gmail.com>
2019-07-21 11:13:52 -0400

    added firebase cdn

---
+++ assets/train.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAqJ8SgswevQto1CYVkuTn4R8t_Pp1b-R8",
+    authDomain: "train-activity-b3a51.firebaseapp.com",
+    databaseURL: "https://train-activity-b3a51.firebaseio.com",
+    projectId: "train-activity-b3a51",
+    storageBucket: "",
+    messagingSenderId: "274136486312",
+    appId: "1:274136486312:web:f8a2c788a2dd68cf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
