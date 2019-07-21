f82cf49b6812ab02d30a91f3d7f8e357533972de
Danny Sanchez <dsanchez910@gmail.com>
2019-07-21 12:39:21 -0400

    added button logic to add triain and variables to grab user input

--- assets/train.js
+++ assets/train.js
@@ -7,7 +7,25 @@ var firebaseConfig = {
     messagingSenderId: "274136486312",
     appId: "1:274136486312:web:f8a2c788a2dd68cf"
   };
-  // Initialize Firebase
+
   firebase.initializeApp(firebaseConfig);
 
   var database = firebase.database();
+
+  
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    const trainName = $("#train-name-input").val().trim();
+    const trainDestination = $("#destination-input").val().trim();
+    const trainFirst = moment($("#first-train-input").val().trim(), "MM/DD/YYYY").format("X");
+    const frequency = $("#frequency-input").val().trim();
+
+  const newTrain = {
+    name: trainName,
+    destination: trainDestination, 
+    firstTrain: trainFirst,
+    frequency = 0
+  };
+
+  
